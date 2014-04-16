a7dd49ae1fefba4e7be2641367a7b0dd222e092b
Flaki <slsoftworks@gmail.com>
2014-04-16 22:45:02 +0200

    Removed compiled modules & updated gitignore to exclude them

--- compiled/app-module.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __moduleName = "app";
-console.debug('Initializing "demo" app...');
-var Demo = require('demo').Demo;
-window.APP = new Demo();
-APP.addDemoGroup('ES6 Features');
-APP.addDemoGroup('Open Web API-s');
-var GeolocationDemo = require('webapi/Geolocation').example;
-GeolocationDemo(APP);
